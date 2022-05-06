3a4e4e8c326b2bcfa892543665d6a0f39509f58d
Darshan-Josiah Barber <darshan@darshancomputing.com>
2022-05-06 16:43:09 -0700

    C!

--- src/impl/x86_64/kernel/main.c
+++ src/impl/x86_64/kernel/main.c
@@ -1,9 +1,14 @@
-
+#define cd 0xa3;
 
 void kernel_main() {
+    prt("C is working great!  Okay!");
+}
+
+void prt(char* s) {
     char* vram = 0xb8000 + 640 + 640 + 640 + 640 + 640 + 320;
-    *vram++ = 0x4f;
-    *vram++ = 0xa3;
-    *vram++ = 0x6b;
-    *vram++ = 0xa3;
+
+    while (*s != 0) {
+        *vram++ = *s++;
+        *vram++ = cd;
+    }
 }
