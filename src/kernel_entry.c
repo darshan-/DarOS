23bd3be3364c15c721b005a33ef2a856e3c97aa9
Darshan-Josiah Barber <darshan@darshancomputing.com>
2022-05-11 03:50:21 -0700

    Super annoying, messy, and finicky (and *really* poorly documented!), but finally seem to have inline assembly working to grab keyboard data and put it in a variable and do something with it.  Seriously have to go to bed now.

--- src/kernel_entry.c
+++ src/kernel_entry.c
@@ -26,6 +26,58 @@ void dumpMem(uint8_t* start, int count) {
     }
 }
 
+struct interrupt_frame
+{
+    uint64_t ip;
+    uint64_t cs;
+    uint64_t flags;
+    uint64_t sp;
+    uint64_t ss;
+};
+
+/*
+        mov al, 0x20
+        out PIC_PRIMARY_CMD, al
+
+ */
+void __attribute__((interrupt)) kbd_irq(struct interrupt_frame *frame) {
+    uint8_t code;
+    __asm__ __volatile__(
+                         "in $0x60, %%al\n"
+                         "mov %%al, %0\n"
+                         "mov $0x20, %%al\n"
+                         "out %%al, $0x20\n"
+                         :"=m"(code)
+    );
+    char* s = "C keyboard interrupt handler... \n";
+    s[31] = code;
+    printColor(s, 0x0d);
+    //__asm__("in $0x60, %al\n"
+    //        "movl %%eax, %0\n" :"=r"(code));
+}
+
+/*
+        mov rax, keyboard_gate
+        mov ebx, idt + (16 * 0x21)
+        mov [ebx], ax
+        shr rax, 16
+        mov [ebx+6], rax
+
+ */
+void set_kbd_irq() {
+    uint16_t* irq1 = (uint16_t*) (16 * 0x21);
+    uint64_t offset = (uint64_t) kbd_irq;
+    *irq1 = (uint16_t) (offset & 0xffff);
+    offset >>= 16;
+    irq1 += 3;
+    *irq1 = (uint16_t) (offset & 0xffff);
+    irq1++;
+    offset >>= 16;
+    *irq1 = (uint32_t) offset;
+
+    //dumpMem(16 * 0x21, 16*3);
+}
+
 void __attribute__((section(".kernel_entry"))) kernel_entry() {
     //clearScreen();
     //print("Kernel launched!\n");
@@ -33,6 +85,7 @@ void __attribute__((section(".kernel_entry"))) kernel_entry() {
 
     //__asm__("int $3");
     printColor("Running 64-bit kernel written in C!\n", 0x0d);
+    set_kbd_irq();
     //__asm__("int $3");
     //__asm__("int $3");
 
