af2aa5bb2e4fcdc32fe8581e842ea4220712d83e
Darshan-Josiah Barber <darshan@darshancomputing.com>
2022-05-30 20:10:04 -0700

    Caps lock.

--- src/keyboard.c
+++ src/keyboard.c
@@ -7,17 +7,18 @@
 // https://www.win.tue.nl/~aeb/linux/kbd/scancodes-1.html
 
 #define si(k) (struct input) {(k), alt_down, ctrl_down, shift_down}
-// Code, char if shift not down, char if shift down
-#define map(c, n, s) case c: gotInput(shift_down ? si(s) : si(n)); break
+#define shifty(c, n, s) case c: gotInput(shift_down ? si(s) : si(n)); break
+#define capsy(c, n, s) case c: gotInput((shift_down && !caps_lock_on) || \
+                                        (!shift_down && caps_lock_on) ? si(s) : si(n)); break
 
 // Let's ignore caps lock for now, but keep track of shift
 static uint8_t shift_down = 0;
 static uint8_t ctrl_down = 0;
 static uint8_t alt_down = 0;
+static uint8_t caps_lock_on = 0; // We really mean 0 as unchanged from what it initially was...
 
 static struct list* inputCallbackList = (struct list*) 0;
 
-// Call for characters that count as input (for now just print them...)
 static inline void gotInput(struct input c) {
     forEachListItem(inputCallbackList, ({
         void __fn__ (void* item) {
@@ -54,62 +55,66 @@ void keyScanned(uint8_t c) {
         alt_down = hob ? 0 : 1;
         break;
 
-        map(0x02, '1', '!');
-        map(0x03, '2', '@');
-        map(0x04, '3', '#');
-        map(0x05, '4', '$');
-        map(0x06, '5', '%');
-        map(0x07, '6', '^');
-        map(0x08, '7', '&');
-        map(0x09, '8', '*');
-        map(0x0a, '9', '(');
-        map(0x0b, '0', ')');
+    case 0x3a:
+        caps_lock_on = !caps_lock_on;
+        break;
+
+        shifty(0x02, '1', '!');
+        shifty(0x03, '2', '@');
+        shifty(0x04, '3', '#');
+        shifty(0x05, '4', '$');
+        shifty(0x06, '5', '%');
+        shifty(0x07, '6', '^');
+        shifty(0x08, '7', '&');
+        shifty(0x09, '8', '*');
+        shifty(0x0a, '9', '(');
+        shifty(0x0b, '0', ')');
         //case 0x0b:
         //__asm__ __volatile__("int $0x23");
         //int n = 1/0;
         //break;
-        map(0x0c, '-', '_');
-        map(0x0d, '=', '+');
+        shifty(0x0c, '-', '_');
+        shifty(0x0d, '=', '+');
 
-        map(0x10, 'q', 'Q');
-        map(0x11, 'w', 'W');
-        map(0x12, 'e', 'E');
-        map(0x13, 'r', 'R');
-        map(0x14, 't', 'T');
-        map(0x15, 'y', 'Y');
-        map(0x16, 'u', 'U');
-        map(0x17, 'i', 'I');
-        map(0x18, 'o', 'O');
-        map(0x19, 'p', 'P');
-        map(0x1a, '[', '{');
-        map(0x1b, ']', '}');
+        capsy(0x10, 'q', 'Q');
+        capsy(0x11, 'w', 'W');
+        capsy(0x12, 'e', 'E');
+        capsy(0x13, 'r', 'R');
+        capsy(0x14, 't', 'T');
+        capsy(0x15, 'y', 'Y');
+        capsy(0x16, 'u', 'U');
+        capsy(0x17, 'i', 'I');
+        capsy(0x18, 'o', 'O');
+        capsy(0x19, 'p', 'P');
+        shifty(0x1a, '[', '{');
+        shifty(0x1b, ']', '}');
 
-        map(0x1c, '\n', '\n');
+        capsy(0x1c, '\n', '\n');
 
-        map(0x1e, 'a', 'A');
-        map(0x1f, 's', 'S');
-        map(0x20, 'd', 'D');
-        map(0x21, 'f', 'F');
-        map(0x22, 'g', 'G');
-        map(0x23, 'h', 'H');
-        map(0x24, 'j', 'J');
-        map(0x25, 'k', 'K');
-        map(0x26, 'l', 'L');
-        map(0x27, ';', ':');
-        map(0x28, '\'', '"');
+        capsy(0x1e, 'a', 'A');
+        capsy(0x1f, 's', 'S');
+        capsy(0x20, 'd', 'D');
+        capsy(0x21, 'f', 'F');
+        capsy(0x22, 'g', 'G');
+        capsy(0x23, 'h', 'H');
+        capsy(0x24, 'j', 'J');
+        capsy(0x25, 'k', 'K');
+        capsy(0x26, 'l', 'L');
+        shifty(0x27, ';', ':');
+        shifty(0x28, '\'', '"');
 
-        map(0x2c, 'z', 'Z');
-        map(0x2d, 'x', 'X');
-        map(0x2e, 'c', 'C');
-        map(0x2f, 'v', 'V');
-        map(0x30, 'b', 'B');
-        map(0x31, 'n', 'N');
-        map(0x32, 'm', 'M');
-        map(0x33, ',', '<');
-        map(0x34, '.', '>');
-        map(0x35, '/', '?');
+        capsy(0x2c, 'z', 'Z');
+        capsy(0x2d, 'x', 'X');
+        capsy(0x2e, 'c', 'C');
+        capsy(0x2f, 'v', 'V');
+        capsy(0x30, 'b', 'B');
+        capsy(0x31, 'n', 'N');
+        capsy(0x32, 'm', 'M');
+        shifty(0x33, ',', '<');
+        shifty(0x34, '.', '>');
+        shifty(0x35, '/', '?');
 
-        map(0x39, ' ', ' ');
+        shifty(0x39, ' ', ' ');
     default:
         break;
     }
