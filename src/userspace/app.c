770c8b0dc5961af8f2c18903778526c7c6f65d3a
Darshan-Josiah Barber <darshan@darshancomputing.com>
2022-06-26 16:50:19 -0700

    Playing with usermode code.

--- src/userspace/app.c
+++ src/userspace/app.c
@@ -1,4 +1,22 @@
-void main() {
-    for (;;)
-        asm volatile("inc %r14");
+#include <stdint.h>
+
+static uint64_t a;
+static uint64_t b;
+
+static uint64_t fibNext() {
+    uint64_t c = a;
+    a = b;
+    b = a + c;
+}
+
+void __attribute__((section(".entry"))) main() {
+    a = 1;
+    b = 1;
+
+    for (;;) {
+        asm volatile("movq %0, %%r14"::"m"(a));
+        asm volatile("movq %0, %%r15"::"m"(b));
+        if (b < 10000000000000000000000ull)
+            fibNext();
+    }
 }
